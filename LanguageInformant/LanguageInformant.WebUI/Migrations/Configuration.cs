b56078c45539d8aac1eb03fdaa30633f4c02927b
cfriedrich <chrisfriedrich@outlook.com>
2014-05-10 21:11:29 -0700

    Revert "Breadcrumbs and UserInfo"
    This reverts commit bea18022023db7063f93a5c7b678d3e69a7e3f0f.

--- LanguageInformant/LanguageInformant.WebUI/Migrations/Configuration.cs
+++ LanguageInformant/LanguageInformant.WebUI/Migrations/Configuration.cs
@@ -10,6 +10,7 @@ namespace LanguageInformant.WebUI.Migrations
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
+            ContextKey = "LanguageInformant.WebUI.Models.ApplicationDbContext";
         }
 
         protected override void Seed(LanguageInformant.WebUI.Models.ApplicationDbContext context)
