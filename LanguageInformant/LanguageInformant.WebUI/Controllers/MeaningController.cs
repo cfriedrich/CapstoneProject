9750a1732c699d5f54c7e46fe4103b01d9cc0921
cfriedrich <chrisfriedrich@outlook.com>
2014-05-29 20:45:08 -0700

    Added Wordle Graphic
    Updated some of the crud interfaces

--- LanguageInformant/LanguageInformant.WebUI/Controllers/MeaningController.cs
+++ LanguageInformant/LanguageInformant.WebUI/Controllers/MeaningController.cs
@@ -14,11 +14,6 @@ namespace LanguageInformant.WebUI.Controllers
     {
         private IMeaningRepository repository = new EFMeaningRepository();
 
-        //public MeaningController(IMeaningRepository meaningRepository)
-        //{
-        //    this.repository = meaningRepository;
-        //}
-
         public ViewResult Index()
         {
             return View();
@@ -83,103 +78,10 @@ namespace LanguageInformant.WebUI.Controllers
             }
         }
 
-        //[HttpPost]
-        //public ActionResult EditMeaning(Meaning meaning, HttpPostedFileBase Picture)
-        //{
-
-        //    //if (ModelState.IsValid)
-        //    //{
-        //    //    //if (Picture != null)
-        //    //    //{
-
-        //            Int32 length = Picture.ContentLength;
-
-        //            //    byte[] tempImage = new byte[length];
-        //            //    Picture.InputStream.Read(tempImage, 0, length);
-
-        //            //    meaning.Picture = tempImage;
-
-        //            //    //string dataurl = Picture.ToString();
-        //            //    //var data = dataurl.Substring(dataurl.IndexOf(",") + 1);
-        //            //    //var newfile = Convert.FromBase64String(data);
-        //            //    //    string bs64OfBytes = Convert.ToBase64String();
-
-
-        //            //    //           // meaning.ImageMimeType = newfile.ContentType;
-        //            //    //            meaning.Picture = new byte[newfile.Length];
-        //            //    //            Picture.InputStream.Read(meaning.Picture, 0, newfile.Length);
-        //            // }
-        //            //            repository.SaveMeaning(meaning);
-        //            //            TempData["message"] = string.Format("{0} has been saved", meaning.Name);
-        //            //            return RedirectToAction("SaveMeaning", "Meaning", meaning);
-        //            //}
-        //            //else
-        //            //{
-        //            //    //        TempData["mesage"] = string.Format("{0} has not been saved", meaning.Name);
-        //            //    //        return RedirectToAction("ListItems", "Item", meaning);
-        //            //    //    }
-        //            //    ////    string thisImage = image.ToString();
-        //            //    ////    thisImage = ToBase64Transform;
-
-        //            //    ////    const string ExpectedImagePrefix = "data:image/jpeg;base64,";
-        //            //    ////    //if (thisImage.StartsWith(ExpectedImagePrefix))
-        //            //    ////    //{
-        //            //    ////    string base64 = thisImage.Substring(ExpectedImagePrefix.Length);
-        //            //    ////    byte[] convertedImage = Convert.FromBase64String(base64);
-
-        //            //    ////    //meaning.ImageMimeType = image.ContentType;
-        //            //    ////    meaning.Picture = convertedImage;
-        //            //    ////    image.InputStream.Read(meaning.Picture, 0, convertedImage.Length);
-        //            //    ////}
-        //            //    ////    repository.SaveMeaning(meaning);
-        //            //        TempData["message"] = string.Format("{0} has been saved", meaning.Name);
-        //            //        return RedirectToAction("SaveMeaning", "Meaning", meaning);
-        //            //    }
-        //            ////else
-        //            ////    {
-
-        //            ////        TempData["mesage"] = string.Format("{0} has not been saved", meaning.Name);
-        //            ////        return RedirectToAction("List", "Meaning", meaning);
-        //            ////        }  
-        //            return View();
-                
-            
-        //}
-
         public ViewResult SaveMeaning(Meaning meaning)
         {
             return View("SaveMeaning", meaning);
         }
-
-        //public FileContentResult GetPicture(int meaningId)
-        //{
-        //    Meaning meaning = repository.GetMeaning(meaningId);
-        //    if (meaning != null)
-        //    {
-        //        return File(meaning.Picture, meaning.ImageMimeType);
-        //    }
-        //    else
-        //    {
-        //        return null;
-        //    }
-        //}
-
-        //[HttpPost]
-        //public ActionResult Edit(Meaning meaning)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        repository.SaveMeaning(meaning);
-        //        TempData["message"] = string.Format("{0} has been saved", meaning.Name);
-        //        return RedirectToAction("List", "Meaning");
-        //    }
-        //    else
-        //    {
-        //        // There is something wrong with the data values
-
-        //        return View(meaning);
-        //    }
-        //}
 
         public ViewResult Delete(int meaningId)
         {
